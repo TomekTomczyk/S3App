afa424da30fa68fd48c231a2a301db8d5e54fb6b
TomekTomczyk <tomasz.m.tomczyk@gmail.com>
2023-08-17 10:26:35 +0200

    SPRINT 1

--- app.py
+++ app.py
@@ -1,30 +1,52 @@
 import boto3
 import requests
+import os
 
-url = 'http://127.0.0.1:5000/getNoOfRecs'
+from flask import Flask, redirect, url_for, request, render_template
 
-num_of_rows = requests.get(url).text
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png'}
 
-print(num_of_rows)
 
-text_file = open("file_to_upload.txt", "w")
-n = text_file.write(num_of_rows)
-text_file.close()
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-OBJECT_NAME_TO_UPLOAD = 'file_to_upload.txt'
 
-s3_client = boto3.client('s3')
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 
-# Generate the presigned URL
-response = s3_client.generate_presigned_post(
-    Bucket='ttomczyk-bucket-ups006',
-    Key=OBJECT_NAME_TO_UPLOAD,
-    ExpiresIn=10
-)
+    @app.route("/", methods=["GET", "POST"])
+    def index():
+        url = os.environ['GET_NUM_OF_RECS_URL']
 
-print(response)
+        users_count = requests.get(url).text
 
-# Upload file to S3 using presigned URL
-files = {'file': open(OBJECT_NAME_TO_UPLOAD, 'rb')}
-r = requests.post(response['url'], data=response['fields'], files=files)
-print(r.status_code)
+        print(users_count)
+
+        text_file = open("file_to_upload.txt", "w")
+        text_file.write(users_count)
+        text_file.close()
+
+        if request.method == "POST":
+            uploaded_file = request.files["file-to-save"]
+            if not allowed_file(uploaded_file.filename):
+                return "FILE NOT ALLOWED!"
+
+            bucket_name = 'ttomczyk-bucket-ups006'
+            s3_client = boto3.client('s3')
+
+            response = s3_client.generate_presigned_post(
+                Bucket=bucket_name,
+                Key=uploaded_file.filename,
+                ExpiresIn=10
+            )
+
+            files = {'file': open(uploaded_file.filename, 'rb')}
+            r = requests.post(response['url'], data=response['fields'], files=files)
+            print(r.status_code)
+
+            return redirect(url_for("index"))
+
+        return render_template("index.html", users_count=users_count)
+
+    return app
